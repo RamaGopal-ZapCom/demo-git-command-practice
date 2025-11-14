b0ebc0b4407507fdfb282034230d0db428a9fa74
RamaGopal-ZapCom <rama.gopal@zapcg.com>
2025-11-14 14:57:49 +0530

    second commit

---
+++ src/main/java/com/example/demo_git_command_practice/DemoGitCommandPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_git_command_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitCommandPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGitCommandPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_git_command_practice/DemoGitCommandPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_git_command_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitCommandPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
